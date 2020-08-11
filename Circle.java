cd7f02ae205bbb792fb919388aefe6f522ba47d7
Karusala <vinaykumar.karusala@dxc.com>
2020-08-11 17:19:01 +0530

    first commit

---
+++ Circle.java
@@ -0,0 +1,12 @@
+public class Circle
+{
+    double radius;
+    public Circle(double radius)
+    {
+	this.radius=radius;
+    }
+    public double area()
+    {
+	return 3.14*radius*radius;
+    }
+}
