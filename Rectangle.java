3f884c434f53678f49ef7c4c9837d55a8a9d341c
vinaykumar612 <69514126+vinaykumar612@users.noreply.github.com>
2020-08-12 10:21:42 +0530

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,4 +1,12 @@
 public class Rectangle
 {
-  double length,breadth;
-  public rectangle
+ double length,breadth;
+ public Rectangle(double lenght,double breadth)
+ {
+    this.length; this.breadth=breadth;
+    }
+     public double area()
+     {
+      return length * breadth;
+      }
+  }   
